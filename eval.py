32a6976c5a6be210ff870fb18120d771e9528960
Tyssel <toni.schmeida@gmail.com>
2020-06-23 14:06:02 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -6,15 +6,16 @@ import plotly
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from pathlib import Path
-path = Path('data\\data_rt50_general.csv')
+path = Path('data\\data_rt50_multi.csv')
 df = pd.read_csv(path, sep = ';', dtype = 'str', index_col=0)
 fig = make_subplots(rows = len(df.columns), cols=1, subplot_titles=df.columns)
 num = 1
 for i in df.columns:
     data = df[i].value_counts(normalize=True)
-    fig.add_trace(go.Bar(x=data.index, y=data.values, name=i), row = num, col =1)
+    x= pd.Series(data.index, dtype=str)
+    fig.add_trace(go.Bar(x=x, y=data.values, name=i), row = num, col =1)
+    fig.update_xaxes(type="category",categoryorder="category ascending", row = num, col =1)
     num = num + 1
-    #if num == 20:
-    #    break
-fig.update_layout(height= num*1000, showlegend=False)
+   
+fig.update_layout(height= num*800, showlegend=False)
 fig.show() 
